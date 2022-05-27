2d97e41901960e06d9dc37366548dc4372c72822
JoelMann <jmann.creston@gmail.com>
2022-05-27 14:12:57 -0700

    pushing to remote for saving.

---
+++ leet/704/sol.py
@@ -0,0 +1,20 @@
+"""
+704. Binary Search
+
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
+
+class Solution:
+    def solve(self, nums: list[int], target: int) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    exit(main())
---
+++ leet/704/test_sol.py
@@ -0,0 +1,13 @@
+import pytest
+from sol import Solution
+
+
+@pytest.mark.parametrize(
+    "test_input, test_target, expected",
+    [
+        ([-1,0,3,5,9,12], 9, 4),
+    ],
+)
+def test_sol(test_input, test_target, expected):
+    s = Solution()
+    assert s.solve(test_input, test_target) == expected
